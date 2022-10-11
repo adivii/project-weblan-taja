b814c7393da17c367ae1c38f962bf0b8584033d9
Adiwijaya Satria Nusantara <adihanyu@gmail.com>
2022-10-11 19:04:31 +0700

    file fixes

--- app/Views/pages/about.php
+++ app/Views/pages/about.php
@@ -1,20 +1,10 @@
 <?= $this->extend('templates/template') ?>
 
-<?= $this->section('head') ?>
-<link rel="stylesheet" href="assets/styles/about.css">
-<?= $this->endSection() ?>
-
 <?= $this->section('content') ?>
 
 <!-- Code Here -->
 
 <div class="container-fluid position-absolute w-100 h-100 p-0">
-    <!-- <div class="container-fluid w-100 h-100 p-0 position-absolute top-0 left-0" style="background:url('assets/images/background-not-found.jpg') no-repeat;background-position: center;">
-        <div class="container-fluid w-100 h-100 top-0 start-0" style="backdrop-filter: brightness(100%);"></div>
-    </div>
-    <div class="container-fluid w-100 h-100 p-0 position-absolute top-0 left-0" style="background-image: linear-gradient(to right, rgba(0,0,0,0.25), rgba(0,0,0,0.5), rgba(0,0,0,0.75), rgba(0,0,0,0.95), rgba(0,0,0,0.95), black);">
-        
-    </div> -->
     <div class="row w-100 h-100">
         <div class="col p-0">
             <div class="container-fluid w-100 h-100 p-0" style="background:url('assets/images/background-not-found.jpg') no-repeat;background-position: center;">
