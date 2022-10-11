aa070206008836e70dab099fb3192c9264548378
ahmadabdansyakuro <ahmadsyakuro164@gmail.com>
2022-10-11 18:53:37 +0700

    tambahkan database migration dan model

---
+++ app/Models/EventModel.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class EventModel extends Model
+{
+    protected $table            = 'events';
+    protected $primaryKey       = 'id';
+    protected $useAutoIncrement = true;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = false;
+    protected $allowedFields    = ['judul_event', 'waktu_event', 'tempat_event', 'created_at', 'updated_at'];
+
+    // Dates
+    protected $useTimestamps = true;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+}
