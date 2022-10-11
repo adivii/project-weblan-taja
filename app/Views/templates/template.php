40bc5e3c19e17403cf46174a9b1b17535fed5766
Adiwijaya Satria Nusantara <adihanyu@gmail.com>
2022-10-11 18:33:44 +0700

    Revert "part gue"
    This reverts commit 888aecb45d2dfc3265e9940c53ccc9f99451ca05.

--- app/Views/templates/template.php
+++ app/Views/templates/template.php
@@ -6,29 +6,39 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TAJA | <?= $title ?></title>
-    <link rel="shortcut icon" href="/assets/images/wheat-icons.png" type="image/x-icon">
+    <link rel="shortcut icon" href="assets/images/wheat-icons.png" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="/styles/fonts.css">
-    <link rel="stylesheet" href="/styles/color.css">
 </head>
 
 <body>
-    <a class="btn position-absolute" style="z-index: 5; top: 10px; left: 10px;" data-bs-toggle="offcanvas" href="#mainSidebar" role="button" aria-controls="offcanvasExample">
-        <img src="/assets/images/menu.png" height="20px">
-    </a>
-    <div class="offcanvas offcanvas-start" tabindex="-1" id="mainSidebar" aria-labelledby="mainSidebarLabel" style="background-color: var(--color-two); border: none;">
-        <div class="offcanvas-header">
-            <h4 class="offcanvas-title" id="mainSidebarLabel" style="color: #fefefe;font-weight: bold;">TAJA</h4>
+    <nav class="navbar navbar-expand-lg fixed-top" style="background-color: transparent;">
+        <div class="container-fluid">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <img src="assets/images/taja-logo-light.png" alt="" height="100px" class="position-absolute">
+                <ul class="navbar-nav ms-auto me-3 mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="/home" style="color: white;">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" style="color: white;">About Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" style="color: white;">Events</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" style="color: white;">Our Services</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" style="color: white;">Feedback</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" style="color: white;">Login</a>
+                    </li>
+                </ul>
+            </div>
         </div>
-        <div class="offcanvas-body">
-            <hr style="height: 3px; border: none; color: #fefefe; background-color: white;">
-            <a class="nav-link ms-3" style="color: #fefefe;" href="/home">Home</a>
-            <hr style="height: 3px; border: none; color: #fefefe; background-color: white;">
-            <a class="nav-link ms-3" style="color: #fefefe;" href="/about">About</a>
-            <hr style="height: 3px; border: none; color: #fefefe; background-color: white;">
-        </div>
-    </div>
+    </nav>
 
     <?= $this->renderSection('content') ?>
 </body>
